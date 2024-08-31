870fd9a82bc12c784b720eff38c354d2cb47c1ba
Varun Hosadurga Harish <varun.hosadurgaharish@stonybrook.edu>
2024-08-31 15:16:28 -0400

    Added disabled when loading

---
+++ resume-parser-portfolio/src/components/Loading.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactLoading from "react-loading";
+
+const Loading = () => {
+  return <ReactLoading height={500} width={1000} />;
+};
+
+export default Loading;
--- resume-parser-portfolio/src/components/Upload.jsx
+++ resume-parser-portfolio/src/components/Upload.jsx
@@ -2,11 +2,13 @@ import React, { useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUpFromBracket, faFileArrowUp } from "@fortawesome/free-solid-svg-icons";
 import { Button } from "./ui/button";
+import Loading from "./Loading";
 
 const Upload = () => {
   const inputFileRef = useRef(null);
   const [fileName, setFileName] = useState(null);
   const [file, setFile] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleFileUpload = (event) => {
     const files = event.target.files;
@@ -26,20 +28,24 @@ const Upload = () => {
     formData.append("uploadedFile", file);
 
     try {
+      setIsLoading(true);
       const resp = await fetch("http://localhost:3000/retrieve-parsed-data", {
         method: "post",
         body: formData,
       });
 
       if (resp.ok) {
-        const result = await resp.json(); 
+        setIsLoading(false);
+        const result = await resp.json();
         console.log("Upload successful:", result);
         setFileName(null);
         setFile(null);
       } else {
+        setIsLoading(false);
         console.error("Upload failed:", resp.statusText);
       }
     } catch (error) {
+      setIsLoading(false);
       console.error("Error uploading file:", error);
     }
   };
@@ -61,6 +67,7 @@ const Upload = () => {
         className="hidden"
         onChange={handleFileUpload}
         name="uploadedFile"
+        disabled={isLoading ? true : false}
       />
       <div
         className="bg-white w-[1000px] h-[250px] flex items-center justify-center -mt-20 border-dashed border-2 border-[#0061ff] cursor-pointer"
@@ -77,7 +84,7 @@ const Upload = () => {
             <Button
               variant="outline"
               className="bg-white gap-2 text-[#0061ff] rounded-lg border-[#0061ff]"
-              type="button" 
+              type="button"
               onClick={handleSendFile} // Trigger file upload
             >
               <span>
